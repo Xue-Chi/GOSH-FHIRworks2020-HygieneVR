9cfa8eb6055a57e1aa8e57617338336085a532a6
薛驰 <xuechi@setsus-mbp.mynet>
2020-03-23 06:05:24 +0000

    init

---
+++ Assets/Scripts/API.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class API : MonoBehaviour
+{
+	private const string URL = "http://localhost:5000/api/Patient/pages/1";
+	public Text responseText;
+
+    public void Request()
+	{
+        WWW request = new WWW(URL);
+		StartCoroutine(OnResponce(request));
+	}
+
+    private IEnumerator OnResponce(WWW req)
+	{
+		yield return req;
+
+		responseText.text = req.text;
+	}
+}
---
+++ Assets/Scripts/LoadJSON.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+using UnityEngine.Networking;
+
+public class LoadJSON : MonoBehaviour
+{
+	public InputField inputTxt;
+	public Text countryTxt;
+	public Text nameTxt;
+	public Text genderTxt;
+	public Text birthTxt;
+
+	public void GetJsonData()
+	{
+		StartCoroutine(RequestWebService());
+	}
+
+	IEnumerator RequestWebService()
+	{
+		string getDataUrl = "http://localhost:5000/api/Patient/" + inputTxt.text;
+		print(getDataUrl);
+
+		using (UnityWebRequest webData = UnityWebRequest.Get(getDataUrl))
+		{
+			yield return webData.SendWebRequest();
+			if (webData.isNetworkError || webData.isHttpError)
+			{
+				print("------------ERROR------------");
+				print(webData.error);
+			}
+			else
+			{
+				if (webData.isDone)
+				{
+					JSONNode jsonData = JSON.Parse(System.Text.Encoding.UTF8.GetString(webData.downloadHandler.data));
+
+					if (jsonData == null)
+					{
+						print("-------------NO DATA-------------");
+					}
+					else
+					{
+						print("-------------JSON DATA-------------");
+						print("jsonData.Count:" + jsonData.Count);
+						nameTxt.text = jsonData["name"][0]["given"][0] + jsonData["name"][0]["family"];
+						genderTxt.text = jsonData["gender"];
+						birthTxt.text = jsonData["birthDate"];
+						countryTxt.text = jsonData["address"][0]["country"];
+					}
+				}
+			}
+		}
+	}
+}
